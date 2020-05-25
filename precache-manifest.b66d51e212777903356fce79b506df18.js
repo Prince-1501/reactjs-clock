de2ccb7ef31562b785fabd0fc9546b946c172c91
Prince-1501 <princekumarrpcsvm10@gmail.com>
2020-05-25 13:35:55 +0530

    Updates

---
+++ precache-manifest.b66d51e212777903356fce79b506df18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1b27a7beb0303c2c64cc6323aec6df",
+    "url": "/Prince-1501/reactjs-clock/index.html"
+  },
+  {
+    "revision": "ba892598468f837a6448",
+    "url": "/Prince-1501/reactjs-clock/static/css/main.8d1b994c.chunk.css"
+  },
+  {
+    "revision": "031084be6ad1bfaf2677",
+    "url": "/Prince-1501/reactjs-clock/static/js/2.6aca7c2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Prince-1501/reactjs-clock/static/js/2.6aca7c2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba892598468f837a6448",
+    "url": "/Prince-1501/reactjs-clock/static/js/main.faec1a35.chunk.js"
+  },
+  {
+    "revision": "4117a831aec40d997ad0",
+    "url": "/Prince-1501/reactjs-clock/static/js/runtime-main.e57ccec0.js"
+  },
+  {
+    "revision": "d94aee2b834b5b711ad16ac2c3dec7a6",
+    "url": "/Prince-1501/reactjs-clock/static/media/bg-image.d94aee2b.svg"
+  }
+]);
